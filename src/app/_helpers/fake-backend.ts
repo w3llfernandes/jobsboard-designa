4a6a4ff5c823eb8051adf20bc0cca9d2efea049f
Well Fernandes <wellbrasil@hotmail.com>
2023-03-09 16:11:11 -0300

    first commit

---
+++ src/app/_helpers/fake-backend.ts
@@ -0,0 +1,99 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { delay, materialize, dematerialize } from 'rxjs/operators';
+
+// array in local storage for registered users
+const usersKey = 'designa-user';
+let users: any[] = JSON.parse(localStorage.getItem(usersKey)!) || [];
+
+@Injectable()
+export class FakeBackendInterceptor implements HttpInterceptor {
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const { url, method, headers, body } = request;
+
+        return handleRoute();
+
+        function handleRoute() {
+            switch (true) {
+                case url.endsWith('/sessions') && method === 'POST':
+                    return authenticate();
+                case url.endsWith('/registrations') && method === 'POST':
+                    return register();
+                default:
+                    // pass through any requests not handled above
+                    return next.handle(request);
+            }    
+        }
+
+        // route functions
+
+        function authenticate() {
+            const { email, password } = body;
+            const user = users.find(x => x.email === email && x.password === password);
+            if (!user) return error('E-mail ou senha incorretos');
+            return ok({
+                ...basicDetails(user),
+                token: 'fake-jwt-token'
+            })
+        }
+
+        function register() {
+            const user = body
+
+            if (users.find(x => x.email === user.email)) {
+                return error('Usuário "' + user.email + '" já existe')
+            }
+
+        
+            users.push(user);
+            localStorage.setItem(usersKey, JSON.stringify(users));
+            return ok();
+        }
+
+        function getUsers() {
+            if (!isLoggedIn()) return unauthorized();
+            return ok(users.map(x => basicDetails(x)));
+        }
+
+
+
+
+
+
+        function ok(body?: any) {
+            return of(new HttpResponse({ status: 200, body }))
+                .pipe(delay(500)); // delay observable to simulate server api call
+        }
+
+        function error(message: string) {
+            return throwError(() => ({ error: { message } }))
+                .pipe(materialize(), delay(500), dematerialize()); // call materialize and dematerialize to ensure delay even if an error is thrown (https://github.com/Reactive-Extensions/RxJS/issues/648);
+        }
+
+        function unauthorized() {
+            return throwError(() => ({ status: 401, error: { message: 'Não autorizado' } }))
+                .pipe(materialize(), delay(500), dematerialize());
+        }
+
+        function basicDetails(user: any) {
+            const { email, name } = user;
+            return { email, name };
+        }
+
+        function isLoggedIn() {
+            return headers.get('Authorization') === 'Bearer fake-jwt-token';
+        }
+
+        function idFromUrl() {
+            const urlParts = url.split('/');
+            return parseInt(urlParts[urlParts.length - 1]);
+        }
+    }
+}
+
+export const fakeBackendProvider = {
+    provide: HTTP_INTERCEPTORS,
+    useClass: FakeBackendInterceptor,
+    multi: true
+};
---
+++ src/app/_services/account.service.ts
@@ -0,0 +1,47 @@
+﻿import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from '@environments/environment';
+import { User } from '@app/_models';
+
+@Injectable({ providedIn: 'root' })
+export class AccountService {
+    private userSubject: BehaviorSubject<User | null>;
+    public user: Observable<User | null>;
+    apiUrl = 'https://jobs-api.jmscarpa.com.br';
+
+    constructor(
+        private router: Router,
+        private http: HttpClient
+    ) {
+        this.userSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('user')!));
+        this.user = this.userSubject.asObservable();
+    }
+
+    public get userValue() {
+        return this.userSubject.value;
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<User>(`${environment.apiUrl}/sessions`, { email, password })
+            .pipe(map(user => {            
+                localStorage.setItem('user', JSON.stringify(user));
+                this.userSubject.next(user);
+                return user;
+            }));
+    }
+
+    logout() {
+        localStorage.removeItem('user');
+        this.userSubject.next(null);
+        this.router.navigate(['/account/login']);
+    }
+
+    register(user: User) {
+        return this.http.post(`${environment.apiUrl}/registrations`, user);
+    }
+
+}
